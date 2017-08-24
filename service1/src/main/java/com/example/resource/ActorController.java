82cee803f5d6b189566782e617c1c7cb86509db8
lalala <davmadrigal85@gmail.com>
2017-08-24 20:49:13 +0200

    Spring - ServerDiscovery and service

---
+++ service1/src/main/java/com/example/resource/ActorController.java
@@ -0,0 +1,38 @@
+package com.example.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Actor;
+
+@RestController
+public class ActorController {
+	
+	@RequestMapping("/actors")
+	public String findActors() {
+		List<Actor> result = new ArrayList<>();
+		result.add(this.buildActor("1", "First1", "Last1"));
+		result.add(this.buildActor("2", "First2", "Last2"));
+		return result.toString();
+		
+	}
+	
+	@RequestMapping("/actors/{id}")
+	public String getActor(@PathVariable("id") String id) {
+		return this.buildActor(id, String.format("First%s", id), String.format("Last%s", id)).toString();
+		
+	}
+
+	
+	private Actor buildActor(String id, String firstName, String lastName) {
+		Actor result = new Actor();
+		result.setActorId(id);
+		result.setFirstName(firstName);
+		result.setLastName(lastName);
+		return result;
+	}
+}
